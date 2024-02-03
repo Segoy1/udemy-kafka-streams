e4157343cf1d716c25fb7a9dbbdd1becb96e9d44
Segoy <segoy.thiel@protonmail.com>
2024-02-03 17:14:49 +0100

    First Stream*

--- word-count/src/main/java/de/segoy/udemy/kafka/streams/StreamsStarterApp.java
+++ word-count/src/main/java/de/segoy/udemy/kafka/streams/StreamsStarterApp.java
@@ -1,8 +1,47 @@
 package de.segoy.udemy.kafka.streams;
 
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
+
 public class StreamsStarterApp {
 
     public static void main(String[] args){
-        System.out.println("Hello");
+
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String,String> wordCountInput = builder.stream("word-count-input");
+
+        KTable<String, Long> wordCounts = wordCountInput.mapValues(value -> value.toLowerCase())
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .selectKey((key, value) -> value)
+                .groupByKey()
+                .count(Named.as("Counts"));
+
+        wordCounts.toStream(Named.as("word-count-output"));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+        System.out.println(streams.toString());
+
+
+        //Shutdown Hook
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
